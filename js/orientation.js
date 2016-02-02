649151fb31e15045fb1904527b8c3e7227a6aa31
grrd01 <gtyedmers@gmx.net>
2016-02-02 21:18:46 +0100

    test

--- js/orientation.js
+++ js/orientation.js
@@ -113,7 +113,7 @@
             document.getElementById("bubble2").setAttribute("transform", transformAttr);
 
             document.getElementById("bubble").setAttribute("cy", ((192.5 - x2) * 0.0714 + 36.29) + "");
-            document.getElementById("bubble2").setAttribute("cy", ((192.5 - x) * 0.0714 + 36.29) + "");
+            document.getElementById("bubble2").setAttribute("cy", ((192.5 + x) * 0.0714 + 36.29) + "");
 
 
         }, false);
