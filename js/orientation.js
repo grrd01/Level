1181c2faf679ab5b573ab5210ca659d8f5c479e7
grrd01 <gtyedmers@gmx.net>
2016-02-10 19:37:33 +0100

    Revert "Update orientation.js"
    This reverts commit 2f9adeaa9b74ef9fb7e91b0e0e325c164034a452.

--- js/orientation.js
+++ js/orientation.js
@@ -1,135 +1,106 @@
-/*
- * Level
- * Copyright (c) 2016 Michele Reho, Tobias Haussener, Gérard Tyedmers
- * Licensed under the MPL License
- */
-
-/*jslint browser:true, devel: true, for: true */
-/*globalModernizr, document, window*/
-
-(function () {
-    "use strict";
-    var ww;
-    var wh;
-    var minWH;
-    var maxWH;
-    var i;
-    var tiltLR;
-    var tiltFB;
-    var y;
-    var y2;
-    var b;
-    var b2;
-    var h;
-    var h2;
-    var x;
-    var x2;
-    var transformAttr;
-
-    /*! modernizr 3.2.0 (Custom Build) | MIT *
-     * http://modernizr.com/download/?-devicemotion_deviceorientation-setclasses !*/
-
-    !function(e,n,o){function s(e,n){return typeof e===n}function i(e){var n=f.className,o=Modernizr._config.classPrefix||"";if(l&&(n=n.baseVal),Modernizr._config.enableJSClass){var s=new RegExp("(^|\\s)"+o+"no-js(\\s|$)");n=n.replace(s,"$1"+o+"js$2")}Modernizr._config.enableClasses&&(n+=" "+o+e.join(" "+o),l?f.className.baseVal=n:f.className=n)}function a(){var e,n,o,i,a,f,l;for(var r in c)if(c.hasOwnProperty(r)){if(e=[],n=c[r],n.name&&(e.push(n.name.toLowerCase()),n.options&&n.options.aliases&&n.options.aliases.length))for(o=0;o<n.options.aliases.length;o++)e.push(n.options.aliases[o].toLowerCase());for(i=s(n.fn,"function")?n.fn():n.fn,a=0;a<e.length;a++)f=e[a],l=f.split("."),1===l.length?Modernizr[l[0]]=i:(!Modernizr[l[0]]||Modernizr[l[0]]instanceof Boolean||(Modernizr[l[0]]=new Boolean(Modernizr[l[0]])),Modernizr[l[0]][l[1]]=i),t.push((i?"":"no-")+l.join("-"))}}var t=[],f=n.documentElement,l="svg"===f.nodeName.toLowerCase(),c=[],r={_version:"3.2.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,n){var o=this;setTimeout(function(){n(o[e])},0)},addTest:function(e,n,o){c.push({name:e,fn:n,options:o})},addAsyncTest:function(e){c.push({name:null,fn:e})}},Modernizr=function(){};Modernizr.prototype=r,Modernizr=new Modernizr,Modernizr.addTest("devicemotion","DeviceMotionEvent"in e),Modernizr.addTest("deviceorientation","DeviceOrientationEvent"in e),a(),i(t),delete r.addTest,delete r.addAsyncTest;for(var d=0;d<Modernizr._q.length;d++)Modernizr._q[d]();e.Modernizr=Modernizr}(window,document);
-    //var Modernizr = Object.create(null);
-    //Modernizr.deviceorientation = true;
-
-    ww = window.innerWidth;
-    wh = window.innerHeight;
-    minWH = Math.min(ww, wh);
-    maxWH = Math.max(ww ,wh);
-
-    //document.getElementById('main').style.height = (maxWH + 50) + "px";
-    //document.getElementById('main').style.width = (minWH + 50) + "px";
-
-    for (i = 0; i < document.getElementsByTagName('svg').length; i += 1) {
-        document.getElementsByTagName('svg')[i].setAttribute("height", ((minWH - 20) / 3.85) + "px");
-        document.getElementsByTagName('svg')[i].setAttribute("width", (minWH - 20) + "px");
-    }
-    document.getElementById('svg2').style.top = (minWH / 2 + minWH / 3.85) + "px";
-
-    if (Modernizr.deviceorientation) {
-        // Listen for the deviceorientation event and handle the raw data
-        window.addEventListener('deviceorientation', function (eventData) {
-            // gamma is the left-to-right tilt in degrees, where right is positive
-            tiltLR = eventData.gamma;
-            // beta is the front-to-back tilt in degrees, where front is positive
-            tiltFB = eventData.beta;
-
-            if (Math.abs(tiltFB) > 80 && Math.abs(tiltFB) < 100) {
-                tiltLR = tiltLR * (Math.abs(90 - Math.abs(tiltFB)) / 10) * (Math.abs(90 - Math.abs(tiltFB)) / 10) * (Math.abs(90 - Math.abs(tiltFB)) / 10);
-            }
-            ww = window.innerWidth;
-            wh = window.innerHeight;
-            minWH = Math.min(ww, wh);
-            maxWH = Math.max(ww ,wh);
-
-            document.getElementById('main').style.height = maxWH + "px";
-            document.getElementById('main').style.width = minWH + "px";
-
-            if (window.orientation === 90) {
-                document.getElementById('main').style.top = ((maxWH - minWH) / -2) + "px";
-                document.getElementById('main').style.left = ((maxWH - minWH) / 2) + "px";
-                document.getElementById('main').style.transform = "rotate(-90deg)";
-            } else if (window.orientation === -90) {
-                document.getElementById('main').style.top = ((maxWH - minWH) / -2) + "px";
-                document.getElementById('main').style.left = ((maxWH - minWH) / 2) + "px";
-                document.getElementById('main').style.transform = "rotate(90deg)";
-            } else if (window.orientation === 0) {
-                document.getElementById('main').style.top = "0";
-                document.getElementById('main').style.left ="0";
-                document.getElementById('main').style.transform = "rotate(0deg)";
-            } else if (window.orientation === 180) {
-                document.getElementById('main').style.top =  "0";
-                document.getElementById('main').style.left =  "0";
-                document.getElementById('main').style.transform = "rotate(180deg)";
-            }
-
-            if (tiltLR < 0) {
-                y = 0 - tiltLR;
-                b = -1;
-            } else {
-                y = tiltLR;
-                b = 1;
-            }
-            if (-90 > tiltFB || tiltFB > 90) {
-                b = b * -1;
-            }
-            h = Math.pow(y, 3 / 4);
-            x = b * h * (180.5 - 44) / Math.pow(90, 3 / 4);
-            transformAttr = ' translate(' + ((192.5 - x) - document.getElementById("bubble").cx.animVal.value) + ',' + 0 + ')';
-            document.getElementById("bubble").setAttribute("transform", transformAttr);
-
-
-            if (tiltFB < 0) {
-                y2 = 0 - tiltFB;
-                b2 = -1;
-            } else {
-                y2 = tiltFB;
-                b2 = 1;
-            }
-
-            if (tiltFB > 90) {
-                y2 = 90 - (tiltFB - 90);
-                b2 = 1;
-            }
-
-            if (tiltFB < -90) {
-                y2 = 0 - (-90 - (tiltFB + 90));
-                b2 = -1;
-            }
-
-            h2 = Math.pow(y2, 3 / 4);
-            x2 = b2 * h2 * (180.5 - 44) / Math.pow(90, 3 / 4);
-            transformAttr = ' translate(' + ((192.5 - x2) - document.getElementById("bubble").cx.animVal.value) + ',' + 0 + ')';
-            document.getElementById("bubble2").setAttribute("transform", transformAttr);
-
-            document.getElementById("bubble").setAttribute("cy", ((192.5 - x2) * 0.0714 + 36.29) + "");
-            document.getElementById("bubble2").setAttribute("cy", ((192.5 + x) * 0.0714 + 36.29) + "");
-
-
-        }, false);
-    } else {
-        document.getElementById("main").innerHTML = "THIS DEVICE DOES NOT SUPPORT DEVICE ORIENTATION";
-    }
-
-}());
+<!DOCTYPE html>
+<!--
+* Level
+* Copyright (c) 2016 Michele Reho, Tobias Haussener, Gérard Tyedmers
+* Licensed under the MPL License
+-->
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+        <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1 , minimum-scale=1.0, maximum-scale=1.0">
+        <meta name="apple-mobile-web-app-capable" content="yes" >
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" >
+        <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57.png" >
+        <link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png" >
+        <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png" >
+        <link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png" >
+        <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon-180x180.png" >
+        <link rel="apple-touch-icon" sizes="192x192" href="img/apple-touch-icon-192x192.png" >
+        <title>Level</title>
+        <link rel="stylesheet" href="css/stylesheet.css">
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" >
+    </head>
+    <body>
+        <div id="main">
+            <svg id="svg1" viewBox="0 0 385 100"  xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <linearGradient id="grad_background_fill" x1="0" x2="0" y1="0" y2="1">
+                        <stop stop-color="#28c800" offset="0"></stop>
+                        <stop stop-color="#7fff00" offset="1"></stop>
+                    </linearGradient>
+                    <linearGradient id="grad_background_stroke" x1="0" x2="0" y1="0" y2="1">
+                        <stop stop-color="#777788" offset="0"></stop>
+                        <stop stop-color="#aaaabb" offset="0.3"></stop>
+                        <stop stop-color="#445566" offset="1"></stop>
+                    </linearGradient>
+                    <linearGradient id="grad_bubble_stroke" y2="1" x2="1" y1="0" x1="0">
+                        <stop stop-color="#000000" offset="0" stop-opacity="1"></stop>
+                        <stop stop-color="#336633" offset="1" stop-opacity="0.3"></stop>
+                    </linearGradient>
+                    <radialGradient id="grad_bubble_fill" cx="50%" cy="50%" r="50%" fx="30%" fy="20%">
+                        <stop offset="0%" style="stop-color:#ffffff; stop-opacity:0.8"></stop>
+                        <stop offset="100%" style="stop-color:#ffffff; stop-opacity:0.3"></stop>
+                    </radialGradient>
+                    <radialGradient id="grad_gloss_top" cx="50%" cy="10%" r="50%" fx="50%" fy="00%">
+                        <stop offset="0%" style="stop-color:rgb(255,255,255);stop-opacity:0.5"></stop>
+                        <stop offset="99%" style="stop-color:rgb(255,255,255);stop-opacity:0.6"></stop>
+                        <stop offset="100%" style="stop-color:rgb(255,255,255);stop-opacity:0"></stop>
+                    </radialGradient>
+                    <linearGradient id="grad_gloss_bottom" x1="0" x2="0" y1="0" y2="1">
+                        <stop stop-color="#ffffff" offset="0.5" stop-opacity="0"></stop>
+                        <stop stop-color="#ffffff" offset="0.5" stop-opacity="0.1"></stop>
+                        <stop stop-color="#ffffff" offset="1" stop-opacity="0.4"></stop>
+                    </linearGradient>
+                </defs>
+                <g>
+                    <rect id="background" height="90" width="375" y="5" x="5" rx="10" ry="10" stroke-width="5" stroke="url(#grad_background_stroke)" fill="url(#grad_background_fill)"></rect>
+                    <ellipse id="bubble" cy="50" cx="192" ry="31" rx="44" stroke-width="2" stroke="url(#grad_bubble_stroke)" fill="url(#grad_bubble_fill)"></ellipse>
+                    <line x1="145" y1="8" x2="145" y2="92" style="stroke:rgb(255, 0, 0);stroke-width:2"></line>
+                    <line x1="240" y1="8" x2="240" y2="92" style="stroke:rgb(255,0,0);stroke-width:2"></line>
+                    <rect id="gloss" height="66" width="371" y="8" x="7" rx="9" ry="9" fill-opacity="0.76" fill="url(#grad_gloss_top)"></rect>
+                    <rect id="gloss2" height="84" width="367" y="7" x="9" rx="8" ry="8" fill-opacity="0.76" fill="url(#grad_gloss_bottom)"></rect>
+                </g>
+            </svg>
+            <svg id="svg2" viewBox="0 0 385 100" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <linearGradient id="grad_background_fill2" x1="0" x2="1" y1="0" y2="0">
+                        <stop stop-color="#28c800" offset="0"></stop>
+                        <stop stop-color="#7fff00" offset="1"></stop>
+                    </linearGradient>
+                    <linearGradient id="grad_background_stroke2" x1="0" x2="1" y1="0" y2="0">
+                        <stop stop-color="#777788" offset="0"></stop>
+                        <stop stop-color="#aaaabb" offset="0.3"></stop>
+                        <stop stop-color="#445566" offset="1"></stop>
+                    </linearGradient>
+                    <linearGradient id="grad_bubble_stroke2" x1="0" x2="1" y1="1" y2="0">
+                        <stop stop-color="#000000" offset="0" stop-opacity="1"></stop>
+                        <stop stop-color="#336633" offset="1" stop-opacity="0.3"></stop>
+                    </linearGradient>
+                    <radialGradient id="grad_bubble_fill2" cx="50%" cy="50%" r="50%" fx="30%" fy="80%">
+                        <stop offset="0%" style="stop-color:#ffffff; stop-opacity:0.8"></stop>
+                        <stop offset="100%" style="stop-color:#ffffff; stop-opacity:0.3"></stop>
+                    </radialGradient>
+                    <radialGradient id="grad_gloss_top2" cx="10%" cy="50%" r="50%" fx="00%" fy="50%">
+                        <stop offset="0%" style="stop-color:rgb(255,255,255);stop-opacity:0.3"></stop>
+                        <stop offset="99%" style="stop-color:rgb(255,255,255);stop-opacity:0.6"></stop>
+                        <stop offset="100%" style="stop-color:rgb(255,255,255);stop-opacity:0"></stop>
+                    </radialGradient>
+                    <linearGradient id="grad_gloss_bottom2" x1="0" x2="1" y1="0" y2="0">
+                        <stop stop-color="#ffffff" offset="0.5" stop-opacity="0"></stop>
+                        <stop stop-color="#ffffff" offset="0.5" stop-opacity="0.1"></stop>
+                        <stop stop-color="#ffffff" offset="1" stop-opacity="0.4"></stop>
+                    </linearGradient>
+                </defs>
+                <g>
+                    <rect id="background2" height="90" width="375" y="5" x="5" rx="10" ry="10" stroke-width="5" stroke="url(#grad_background_stroke2)" fill="url(#grad_background_fill2)"></rect>
+                    <ellipse id="bubble2" cy="50" cx="192" ry="31" rx="44" stroke-width="2" stroke="url(#grad_bubble_stroke2)" fill="url(#grad_bubble_fill2)"></ellipse>
+                    <line x1="145" y1="8" x2="145" y2="92" style="stroke:rgb(255, 0, 0);stroke-width:2"></line>
+                    <line x1="240" y1="8" x2="240" y2="92" style="stroke:rgb(255,0,0);stroke-width:2"></line>
+                    <rect id="gloss3" height="86" width="325" y="8" x="7" rx="9" ry="9" fill-opacity="0.76" fill="url(#grad_gloss_top2)"></rect>
+                    <rect id="gloss4" height="84" width="367" y="7" x="9" rx="8" ry="8" fill-opacity="0.76" fill="url(#grad_gloss_bottom2)"></rect>
+                </g>
+            </svg>
+        </div>
+        <script src="js/orientation.js"></script>
+    </body>
+</html>
